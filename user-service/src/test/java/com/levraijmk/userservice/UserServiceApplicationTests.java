4f32c01d06272166ea096cbc7645c721e2b17c7b
Joelmk28 <joelmuhindok@gmail.com>
2026-05-13 23:36:50 +0100

    first commit

---
+++ user-service/src/test/java/com/levraijmk/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levraijmk.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
